fc25bd87226bf2bef35965d6a2e51545c7a15cce
majimun <yanbarumajimun@gmail.com>
2023-02-03 18:58:47 +0900

    ステージ作成（マップデータ）追加

--- Assets/Scripts/Stage/StageManager.cs
+++ Assets/Scripts/Stage/StageManager.cs
@@ -4,30 +4,48 @@ using UnityEngine;
 
 public class StageManager : MonoBehaviour
 {
+    [System.Serializable]
+    public class BlockMapSetting
+    {
+        public string[] mapDataPath;
+        public string[] heightDataPath;
+    }
+
+
     public GameObject[] fieldblocks;
     public Vector3 blockScale = new Vector3(0, 0, 0);
+    public BlockMapSetting mapSetting;
 
-    private int[,] _mapZX =
-    {
-        { 0, 0, 0 },
-        { 0, 0, 0 },
-        { 0, 0, 0 },
-        { 0, 0, 0 }
-    };
-
-
-    private float[,] _mapHeight =
-    {
-        { 0.5f, 0.5f, 0.5f },
-        { 0.5f, 0f, 0.5f },
-        { 0.5f, 0f, 0.5f },
-        { 0.5f, 0.5f, 0.5f }
-    };
+    private int[,] _mapZX;
+    private float[,] _mapHeight;
 
 
     void Start()
     {
+        Load(mapSetting.mapDataPath[0], mapSetting.heightDataPath[0]);
         Create();
+
+        blockScale.z = (1 * ((Mathf.Sqrt(3) / 2))) * 2;
+    }
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="filePath">ファイルパス（ローカル）</param>
+    public void Load(string mapPath, string heightPath)
+    {
+        string[,] mapData = CSVIO.Read(mapPath);
+        string[,] heightData = CSVIO.Read(heightPath);
+
+        for (int x = 0; x < _mapZX.GetLength(1); x++)
+        {
+            for (int z = 0; z < _mapZX.GetLength(0); z++)
+            {
+                _mapZX[z, x] = int.Parse(mapData[z, x]);
+                _mapHeight[z, x] = int.Parse(heightData[z, x]);
+            }
+        }
     }
 
 
@@ -43,10 +61,8 @@ public class StageManager : MonoBehaviour
                 GameObject block = Instantiate(
                     fieldblocks[_mapZX[z, x]],
                     Vector3.zero + new Vector3(x * blockScale.x, _mapHeight[z, x] * blockScale.y, (zOffset + z) * blockScale.z),
-                    Quaternion.identity
+                    Quaternion.Euler(0, 90, 0)
                     );
-
-                //block.transform.localScale = blockScale;
             }
         }
     }
